edfec40b637e5a56d64ebc64ec506e0a9afaa2e3
AaronCordero-VDM <aaronciempo@gmail.com>
2023-09-26 10:02:50 +0200

    suma

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@ function calcular(){
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
 
-    let sol = parseFloat(num1)-parseFloat(num2);
+    let sol = parseFloat(num1)+parseFloat(num2);
 
     document.getElementById("sol").textContent = sol;
 }
