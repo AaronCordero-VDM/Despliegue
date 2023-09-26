2695798394f9acd4aa1b1e2dff81226b1ebb3956
AaronCordero-VDM <aaronciempo@gmail.com>
2023-09-26 13:22:37 +0200

    ACABADO

--- calc.js
+++ calc.js
@@ -1,4 +1,22 @@
-function calcular(){
+function resta(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let sol = parseFloat(num1)-parseFloat(num2);
+
+    document.getElementById("sol").textContent = sol;
+}
+
+function suma(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let sol = parseFloat(num1)+parseFloat(num2);
+
+    document.getElementById("sol").textContent = sol;
+}
+
+function multiplicacion(){
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
 
